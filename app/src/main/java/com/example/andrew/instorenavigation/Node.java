54687500f674167bdf32507d3b8f1bc0e5e942ef
Matthew Catron <mc06579@georgiasouthern.edu>
2017-11-10 21:39:22 -0500

    Added the current in progress algorithm code. Need edit Path still, node should be fine as long as the information is parsed before hand

--- app/src/main/java/com/example/andrew/instorenavigation/Node.java
+++ app/src/main/java/com/example/andrew/instorenavigation/Node.java
@@ -1,14 +1,49 @@
 package com.example.andrew.instorenavigation;
 
+import java.util.ArrayList;
+
 /**
  * Created by Andrew on 11/9/17.
  * Responsible for creating nodes for the Path class to run it's algorithm
  */
 
-public class Node implements Presenter {
+public class Node {
 
-    @Override
-    public String[] JSONParser() {
-        return new String[0];
+    private ArrayList<Integer> edges;
+
+    //Limit will always be two because we only care to come to the node once, and leave the node once,
+    //barring the start node (left once) and the end node (come to once).
+    private int limit = 2;
+
+    //This is to check to make sure that a node has reached its limit and therefore has been visited and
+    //will not be visited again. Set to true once the condition is met.
+    private boolean atCap = false;
+
+    //This is to tell you which node you are coming from and going to, on the users end, we will be showing
+    //the going to as the next item for them to get
+    private ArrayList<Node> connections;
+
+    //Instantiate the node with the limit (2) and create the edge and connection lists
+    Node() {
+        edges = new ArrayList<Integer>();
+        connections = new ArrayList<Node>();
+    }
+
+    //This is going to be used to calculate what the shortest path between two product nodes is
+    //will pull the different edges from the database that can connect to the target node from
+    //the source node.
+    public void addEdge(int edge, Node node) {
+        edges.add(edge);
+        connections.add(node);
+        if (edges.size() == limit) {
+            atCap = true;
+        }
+    }
+
+    //Return the atCap boolean to the algorithm so that it knows whether or not the node can be navigated to.
+    //This boolean is not returne to the algorithm in the instance that the user is passing over to get to
+    //another node.
+    public boolean atCap() {
+        return atCap;
     }
 }
--- app/src/main/java/com/example/andrew/instorenavigation/Path.java
+++ app/src/main/java/com/example/andrew/instorenavigation/Path.java
@@ -1,14 +1,118 @@
 package com.example.andrew.instorenavigation;
 
+import java.util.ArrayList;
+import java.util.StringJoiner;
+
 /**
  * Created by Andrew on 11/9/17.
+ * Will parse JSON information and pass it to the node class so that nodes can be generated to
+ * calculate a path.
  */
 
-public class Path implements Presenter {
+public class Path {
 
-    @Override
-    public String[] JSONParser() {
-        return new String[0];
+    private int nodeCount;
+    private ArrayList<String[]> nodeEdgeData;
+
+    //The data from the JSON file in the database will return an arraylist of the following format:
+    //      {[NaX, NaY, NbX, NbY, L],[NbX, NbY, NcX, NcY, L],...}
+    //Which is listing the X and Y of connected nodes and the length between them. The collection
+    // contains the nodes needed to travel between a start and end node.
+
+    Path(ArrayList<String[]> nodeEdgeData) {
+        this.nodeEdgeData = nodeEdgeData;
     }
 
+    public void run() {
+
+        this.calculatePath();
+    }
+
+    //int limit is the degree limitation (in our case, always 2)
+    private void calculatePath() {
+
+        //generate a matrix to store the nodes and distance in
+        int[][] matrix = new int[this.nodeCount][this.nodeCount];
+
+        int[] parent = new int[this.nodeCount];
+        int min;
+        int u = 0;
+        int v = 0;
+        int edgeCount = 0;
+
+        Node[] nodes = new Node[nodeCount];
+
+        for (int i = 0; i < nodeCount; i++) {
+            nodes[i] = new Node();
+        }
+
+        //Fill matrix with edge values
+        for (int i = 0; i < nodeCount; i++) {
+            parent[i] = 0;
+            for (int j = i; j < nodeCount; j++) {
+                matrix[i][j] = 0;
+                //If edge is of 0, forced it to be inifinity (relatively large int)
+                if (matrix[i][j] == 0) {
+                    matrix[i][j] = 999;
+                }
+                //if nodes were the same, then there was no edge
+                if (i == j) {
+                    matrix[i][j] = 999;
+                    //inverse relationship held the same edge value (dist between i and j == dist between j and i)
+                } else {
+                    matrix[j][i] = matrix[i][j];
+                }
+            }
+        }
+
+        //Print out the matrix
+        for (int i = 0; i < nodeCount; i++) {
+            System.out.println();
+            for (int j = 0; j < nodeCount; j++) {
+                System.out.printf("%4s", matrix[i][j]);
+            }
+        }
+        System.out.println("\n");
+
+        //Continue loop until path is found
+        while (edgeCount - 1 < nodeCount) {
+            min = 999;
+            for(int i = 0; i < nodeCount; i++) {
+                if (!nodes[i].atCap()) {
+                    for(int j = i; j < nodeCount; j++) {
+                        if (!nodes[j].atCap()) {
+                            if (matrix[i][j] < min) {
+                                min = matrix[i][j];
+                                u = i;
+                                v = j;
+                            }
+                        }
+                    }
+                }
+            }
+            int temp1 = u;
+            int temp2 = v;
+
+            matrix[u][v] = matrix[v][u] = 999;
+
+            //These while loops are used to find the root of the tree and set it as the parent of the newly connected node
+            //this is used to find the root element of each node tree
+            while(parent[u] != 0) {
+                u = parent[u];
+            }
+            while(parent[v] != 0) {
+                v = parent[v];
+            }
+
+            //Check to see that the root of each tree is not the same, if so, connect the two trees with the edge.
+            if (v != u) {
+                if (!nodes[temp1].atCap()) {
+                    nodes[temp1].addEdge(min, nodes[temp2]); nodes[temp2].addEdge(min, nodes[temp1]);
+                    edgeCount++;
+                    System.out.println("Edge Found: " + temp1 + " <-> " + temp2 + " Weight: " + min);
+                }
+                parent[v] = u;
+            }
+        }
+    }
 }
