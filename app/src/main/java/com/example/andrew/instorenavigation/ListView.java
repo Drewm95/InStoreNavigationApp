2c5f034cecc33a861db6a4b6a57ec22d2c6d193b
Andrew <drewmolloy2@gmail.com>
2017-11-18 14:24:48 -0500

    Changed the layout of the list view. Delete button no longer covers text and text view extended.

--- app/src/main/java/com/example/andrew/instorenavigation/ListView.java
+++ app/src/main/java/com/example/andrew/instorenavigation/ListView.java
@@ -2,15 +2,14 @@
 package com.example.andrew.instorenavigation;
 
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
