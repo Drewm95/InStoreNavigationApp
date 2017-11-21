45c15d57b635b64e69df3e97c21d8417ec785d59
Matthew Catron <mc06579@georgiasouthern.edu>
2017-11-21 17:06:37 -0500

    Finalized Store View, need to debug Path

--- app/src/main/java/com/example/andrew/instorenavigation/StoreView.java
+++ app/src/main/java/com/example/andrew/instorenavigation/StoreView.java
@@ -8,7 +8,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.*;
+import android.widget.ListView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -26,14 +27,16 @@ import java.util.Map;
  */
 
 public class StoreView extends AppCompatActivity {
+    ArrayAdapter<String> mAdapter;
+    android.widget.ListView storeNames;
+
     private String listID;
-    private static String store;
     private String storeID;
     String listName;
     String UID;
 
+    private ArrayList<String> stores;
     private Context context;
-    private String StoreID;
     private ArrayList<String> products;
     private String start;
 
@@ -43,13 +46,36 @@ public class StoreView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_view);
         context = this;
+        products = new ArrayList<>();
+        start = "";
+        stores = new ArrayList<>();
+        listID = "";
+        storeNames = findViewById(R.id.storeList);
 
         Intent load = getIntent();
         listName = load.getStringExtra("ListName");
-        UID = load.getStringExtra("userID");
+        UID = load.getStringExtra("UserID");
         super.setTitle("Select Store: " + listName);
 
-        queryStores();
+        queryItems();
+    }
+
+    private void loadTaskList() {
+
+        try {
+            if (mAdapter == null) {
+                mAdapter = new ArrayAdapter<String>(this, R.layout.generate_store_view, R.id.store_title, stores);
+                storeNames.setAdapter(mAdapter);
+            } else {
+                mAdapter = new ArrayAdapter<String>(this, R.layout.generate_store_view, R.id.store_title, stores);
+                storeNames.setAdapter(mAdapter);
+                mAdapter.notifyDataSetChanged();
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     //Method will use a queryNodes to select the stores that hold all of the products.
@@ -85,8 +111,8 @@ public class StoreView extends AppCompatActivity {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("listID", listName);
-                params.put("User_ID", UID);
+                params.put("List_Name", listName);
+                params.put("Users_UserID", UID);
 
                 return params;
             }
@@ -107,7 +133,7 @@ public class StoreView extends AppCompatActivity {
                         Log.d("Response", response);
 
                         if(response.length() > 1){
-                            parseStores(response);
+                            Path path = new Path(response, products, start, context);
                         }
                     }
                 },
@@ -130,8 +156,74 @@ public class StoreView extends AppCompatActivity {
         queue.add(postRequest);
     }
 
-    public void parseStores(String stores) {
+    private void queryItems() {
+        //Connect to the database and authenticate
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String responseValue = null;
 
+
+        String url = "http://34.238.160.248/GetListContent.php";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+
+                        if(response.length() >= 1){
+                            parseItemNames(response);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("List_Name", listName);
+                params.put("Users_UserID", UID);
+                //  params.put("Product_Name", items.);
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+    }
+
+    public void parseStores(String stores) {
+            int i = 0;
+
+            for (int j = 0; j < stores.length(); j++) {
+                String check = stores.substring(j,j+1);
+                if (check.equals(",")) {
+                    String temp = stores.substring(i,j);
+                    this.stores.add(temp);
+                    i = j+1;
+                }
+            }
+
+            loadTaskList();
+    }
+
+    private void parseItemNames(String items) {
+        int i = 0;
+
+        for (int j = 0; j < items.length(); j++) {
+            String check = items.substring(j,j+1);
+            if (check.equals(",")) {
+                String temp = items.substring(i,j);
+                this.products.add(temp);
+                i = j+1;
+            }
+        }
+
+        queryStores();
     }
 
     public void path(final View v) {
@@ -146,12 +238,11 @@ public class StoreView extends AppCompatActivity {
                         Log.e("String", (String) taskTextView.getText());
                         String task = String.valueOf(taskTextView.getText());
 
-                        Path path = new Path(storeID, products, start, context);
+                        queryStoreID(task);
                     }
                 })
                 .setNegativeButton("No",null)
                 .create();
         dialog.show();
     }
-
 }
